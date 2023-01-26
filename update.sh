c9cc34e2ebc40cd0765fcce0508ea74e7f1d8bc0
Exerosis <exerosis@gmail.com>
2023-01-25 23:37:21 -0500

    ~ Adding cloudlab scripts.

---
+++ update.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+git pull
+make build
+sudo ./bin/etcd --log-level panic \
+--initial-cluster infra0=http://192.168.1.1:2380,infra1=http://192.168.1.2:2380,infra2=http://192.168.1.3:2380 \
+--initial-cluster-state new
+
+
+
