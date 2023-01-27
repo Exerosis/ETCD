4c1241a3efa9dfca2d655951b5862828dd7ca450
Exerosis <exerosis@gmail.com>
2023-01-26 22:49:20 -0500

    ~ Adding cloudlab scripts.

--- update.sh
+++ update.sh
@@ -22,10 +22,10 @@ make build
 sudo ./bin/etcd --log-level panic \
 --name "$HOST" \
 --initial-cluster-token etcd-cluster-1 \
---initial-advertise-peer-urls http://"$IP":2380 \
 --listen-client-urls http://"$IP":2379 \
 --advertise-client-urls http://"$IP":2379 \
---listen-peer-urls http://"$IP":12380 \
+--initial-advertise-peer-urls http://"$IP":2380 \
+--listen-peer-urls http://"$IP":2380 \
 --initial-cluster node-1=http://192.168.1.1:2380,node-2=http://192.168.1.2:2380,node-3=http://192.168.1.3:2380 \
 --initial-cluster-state new
 
