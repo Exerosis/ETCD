6fdf4649f09cb90611988e7682679f9fb85909cd
Exerosis <exerosis@gmail.com>
2023-01-26 22:32:46 -0500

    ~ Adding cloudlab scripts.

--- update.sh
+++ update.sh
@@ -6,15 +6,24 @@ git pull
 cd ../ETCD || exit
 git pull
 
+HOST=$(hostname | awk -F "." '{print $1}')
+if [ $HOST == "node-1" ]; then
+    IP="192.168.1.1"
+elif [ $HOST == "node-2" ]; then
+    IP="192.168.1.2"
+elif [ $HOST == "node-3" ]; then
+    IP="192.168.1.3"
+fi
+
 make build
 sudo ./bin/etcd --log-level panic \
---name infra0 \
+--name "$HOST" \
 --initial-cluster-token etcd-cluster-1 \
---initial-advertise-peer-urls http://192.168.1.1:12380 \
---listen-client-urls http://192.168.1.1:2379 \
---advertise-client-urls http://192.168.1.1:2379 \
---listen-peer-urls http://192.168.1.1:12380 \
---initial-cluster infra0=http://192.168.1.1:2380,infra1=http://192.168.1.2:2380,infra2=http://192.168.1.3:2380 \
+--initial-advertise-peer-urls http://"$IP":12380 \
+--listen-client-urls http://"$IP":2379 \
+--advertise-client-urls http://"$IP":2379 \
+--listen-peer-urls http://"$IP":12380 \
+--initial-cluster node-1=http://192.168.1.1:2380,node-2=http://192.168.1.2:2380,node-3=http://192.168.1.3:2380 \
 --initial-cluster-state new
 
 
