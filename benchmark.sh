1e9118d53eaa2a482f5134167fe5292c7741de75
Exerosis <exerosis@gmail.com>
2023-02-17 02:16:17 -0500

    ~ Adding cloudlab scripts.

--- benchmark.sh
+++ benchmark.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 go run ./tools/benchmark \
---endpoints=http://192.168.1.2:2379,http://192.168.1.1:2379,http://192.168.1.3:2379 \
---conns=1 --clients=50 put --key-size=8 --sequential-keys --total=150 --val-size=256
+--endpoints=http://192.168.1.2:2379,http://192.168.1.3:2379 \
+--conns=1 --clients=50 put --key-size=8 --sequential-keys --total=100 --val-size=256
