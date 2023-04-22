39228f0f84f8c4f45ebea867cba4efb3342cc937
Home <exerosis@gmail.com>
2023-04-21 22:40:06 -0400

    ~ Please WSL work.

--- benchmark.sh
+++ benchmark.sh
@@ -2,4 +2,4 @@
 go run ./tools/benchmark \
 --endpoints=http://192.168.1.2:2379,http://192.168.1.3:2379 \
 --conns=1 --clients=40 put --key-size=8 --total=10000 --val-size=256
-#--sequential-keys
+#--sequential-keyssadf
