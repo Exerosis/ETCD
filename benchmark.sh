bbda97aeb894aaf161686961aedb29f0bc17347c
Home <exerosis@gmail.com>
2023-04-23 16:26:10 -0400

    ~ Benchmarking etcd.

--- benchmark.sh
+++ benchmark.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 go run ./tools/benchmark \
 --endpoints=http://192.168.1.2:2379,http://192.168.1.3:2379 \
---conns=1 --clients=40 put --key-size=8 --total=10000 --val-size=256
+--conns=100 --clients=1000 put --key-size=8 --total=10000 --val-size=256
 #--sequential-keyssadf
