3b6b5e3fa1a1397dc8b03fd93fdbc42984a6ab78
Home <exerosis@gmail.com>
2023-06-07 21:07:05 -0400

    ~ Benchmarking.

--- benchmark.sh
+++ benchmark.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 go run ./tools/benchmark \
 --endpoints=http://192.168.1.1:2379,http://192.168.1.2:2379,http://192.168.1.3:2379 \
---conns=5000 --clients=5000 put --key-size=8 --total=100000 --val-size=256
+--conns=5000 --clients=2000 put --key-size=8 --total=100000 --val-size=256
 #--sequential-keyssadf
