e90f59c0cd7396a6c499cc2c4badf3425160263b
Home <exerosis@gmail.com>
2023-06-12 17:40:19 -0400

    ~ Benchmarking.

--- benchmark.sh
+++ benchmark.sh
@@ -4,4 +4,4 @@ HOST_2="http://192.168.1.2:2379"
 HOST_3="http://192.168.1.3:2379"
 go run ./tools/benchmark \
 --endpoints=${HOST_1} --conns=100 --clients=1000 \
-    put --key-size=8 --sequential-keys --total=100000 --val-size=256
+    range YOUR_KEY --consistency=l --total=100000
