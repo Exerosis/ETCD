67e37b5ebbffd385cbcec163405b05ef29939ad8
money <lucasphone75@gmail.com>
2023-12-14 20:40:17 -0700

    set large snapshot count

--- server/embed/etcd.go
+++ server/embed/etcd.go
@@ -224,6 +224,7 @@ func StartEtcd(inCfg *Config) (e *Etcd, err error) {
 		ExperimentalMaxLearners:                       cfg.ExperimentalMaxLearners,
 		V2Deprecation:                                 cfg.V2DeprecationEffective(),
 	}
+	srvcfg.SnapshotCount = 1000000
 
 	if srvcfg.ExperimentalEnableDistributedTracing {
 		tctx := context.Background()
