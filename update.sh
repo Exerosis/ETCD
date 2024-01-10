9a675484c8a9eaf62c2d646c7320129c92209def
Exerosis <exerosis@gmail.com>
2024-01-10 06:15:58 -0500

    ~ Testing.

--- update.sh
+++ update.sh
@@ -42,7 +42,7 @@ else
   export RS_RABIA="false"
 fi
 
-if ["$1" = ""]; then
+if [ "$1" = "" ]; then
   export SETUP="--initial-cluster node-1=http://10.10.1.1:12380,node-2=http://10.10.1.2:12380,node-3=http://10.10.1.3:12380,node-4=http://10.10.1.4:12380"
 fi
 
