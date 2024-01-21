27bfddbdc0f944cabbf7114f41123a1068a31adb
money <lucasphone75@gmail.com>
2024-01-21 00:54:43 -0700

    rabia write debugs

--- server/etcdserver/v3_server.go
+++ server/etcdserver/v3_server.go
@@ -459,6 +459,7 @@ func (s *EtcdServer) Put(ctx context.Context, r *pb.PutRequest) (*pb.PutResponse
 		return s.PineapplePut(ctx, r)
 	}
 	if RS_RABIA {
+		println("Rabia put")
 		return s.RabiaPut(ctx, r)
 	}
 	return s.RaftPut(ctx, r)
