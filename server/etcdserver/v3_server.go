220caafb5c7469606f6dc8640c8b741a11e6dc6a
money <lucasphone75@gmail.com>
2024-01-24 04:25:44 -0700

    add encoding back to paxos

--- server/etcdserver/v3_server.go
+++ server/etcdserver/v3_server.go
@@ -145,22 +145,12 @@ func (s *EtcdServer) PaxosGet(ctx context.Context, r *pb.RangeRequest) (*pb.Rang
 }
 
 func (s *EtcdServer) PaxosPut(r *pb.PutRequest) (*pb.PutResponse, error) {
-	//s.paxos.Write(r.Key, r.Value, func(key []byte, value []byte) {
-	//	trace := traceutil.Get(context.TODO())
-	//	var write = s.KV().Write(trace)
-	//	write.Put(key, value, 0)
-	//	write.End()
-	//})
-
-	trace := traceutil.Get(context.TODO())
-	var write = s.KV().Write(trace)
-	write.Put(r.Key, r.Value, 0)
-	write.End()
-	//
-	//trace := traceutil.Get(context.Background())
-	//var write = s.KV().Write(trace)
-	//write.Put(key, value, 0)
-	//write.End()
+	s.paxos.Write(SEGMENTS, PARITY, r.Key, r.Value, func(key []byte, value []byte) {
+		trace := traceutil.Get(context.TODO())
+		var write = s.KV().Write(trace)
+		write.Put(key, value, 0)
+		write.End()
+	})
 
 	return &pb.PutResponse{
 		Header: &pb.ResponseHeader{},
