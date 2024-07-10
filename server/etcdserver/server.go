a38e7a2a3061c6bd443750348aac8a1eae80b726
Home <exerosis@gmail.com>
2024-07-10 04:19:59 -0400

    ~ Wow that was a stupid bug.

--- server/etcdserver/server.go
+++ server/etcdserver/server.go
@@ -302,7 +302,8 @@ func (racos *Racos) QuorumRead(id uint64) ([]byte, error) {
 	group.Wait()
 	println("total count: ", atomic.LoadUint32(&count))
 	println("Parity", PARITY)
-	segments[3] = nil
+	segments[0] = nil
+	segments[4] = nil
 	var err = racos.encoder.ReconstructData(segments)
 	if err != nil {
 		return nil, err
