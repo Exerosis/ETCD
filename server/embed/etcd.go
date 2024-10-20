e4d9fbb126edccc809294b9e2ac59a67b0e14fb6
money <lucasphone75@gmail.com>
2024-10-19 18:23:35 -0600

    test just disk writes

--- server/embed/etcd.go
+++ server/embed/etcd.go
@@ -186,7 +186,7 @@ func StartEtcd(inCfg *Config) (e *Etcd, err error) {
 		AutoCompactionRetention:                  autoCompactionRetention,
 		AutoCompactionMode:                       cfg.AutoCompactionMode,
 		QuotaBackendBytes:                        20000000000,
-		BackendBatchLimit:                        3000,
+		BackendBatchLimit:                        1,
 		BackendFreelistType:                      backendFreelistType,
 		BackendBatchInterval:                     cfg.BackendBatchInterval,
 		MaxTxnOps:                                cfg.MaxTxnOps,
