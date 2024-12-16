11c088812be417a931842db5fbb77d7bff040cd3
money <lucasphone75@gmail.com>
2024-12-15 17:01:51 -0700

    disable batching and test if syncing

--- server/storage/backend.go
+++ server/storage/backend.go
@@ -50,7 +50,7 @@ func newBackend(cfg config.ServerConfig, hooks backend.Hooks) backend.Backend {
 		// permit 10% excess over quota for disarm
 		bcfg.MmapSize = uint64(cfg.QuotaBackendBytes + cfg.QuotaBackendBytes/10)
 	}
-	fmt.Printf("Bathc interval?: %d\n", bcfg.BatchInterval)
+	fmt.Printf("Bathc interval?: %d\n", bcfg.BatchLimit)
 	bcfg.Mlock = cfg.ExperimentalMemoryMlock
 	bcfg.Hooks = hooks
 	return backend.New(bcfg)
