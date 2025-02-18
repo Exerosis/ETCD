8e116921c61a99395494c398c18fa10ddee892f1
Lucas <lucasphone75@gmail.com>
2025-02-17 20:33:19 -0700

    add in memory kv

--- server/etcdserver/server.go
+++ server/etcdserver/server.go
@@ -772,7 +772,7 @@ func NewServer(cfg config.ServerConfig) (srv *EtcdServer, err error) {
 		return nil, err
 	}
 
-	mvccStoreConfig := mvcc.StoreConfig{
+	_ = mvcc.StoreConfig{
 		CompactionBatchLimit:    cfg.CompactionBatchLimit,
 		CompactionSleepInterval: cfg.CompactionSleepInterval,
 	}
@@ -780,7 +780,7 @@ func NewServer(cfg config.ServerConfig) (srv *EtcdServer, err error) {
 	kv := mvcc.MemoryKV{}
 
 	srv.kv = &kv
-	srv.kv = mvcc.New(srv.Logger(), srv.be, srv.lessor, mvccStoreConfig)
+	//srv.kv = mvcc.New(srv.Logger(), srv.be, srv.lessor, mvccStoreConfig)
 
 	//if pineapple is selected on startup it runs this to start he node
 	if RS_PAXOS {
