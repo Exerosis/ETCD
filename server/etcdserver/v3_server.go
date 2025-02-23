8677a97679016114cd795415db8e3f72dba6486c
Lucas <lucasphone75@gmail.com>
2025-02-23 04:08:42 -0700

    switch to paxos

--- server/etcdserver/v3_server.go
+++ server/etcdserver/v3_server.go
@@ -133,7 +133,6 @@ func (s *EtcdServer) PaxosPut(r *pb.PutRequest) (*pb.PutResponse, error) {
 	//var write = s.KV().Write(trace)
 	//write.Put(r.Key, r.Value, 0)
 	//write.End()
-	println("Calls this?!")
 	return &pb.PutResponse{
 		Header: &pb.ResponseHeader{},
 		PrevKv: &mvccpb.KeyValue{
